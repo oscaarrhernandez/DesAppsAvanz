66a398441853f5b5cd96d52fc28139858825432a
Oscar Hernandez <66411411+oscaarrhernandez@users.noreply.github.com>
2023-09-22 13:36:14 +0200

    Add files via upload

---
+++ MultiTabsApp/MultiTabsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiTabsApp
+//
+//  Created by alumno on 22/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
